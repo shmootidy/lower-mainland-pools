feff14e5e05c3e0d3c40fa0d4ab2e4ab48428022
shmoo <shmooritchie@gmail.com>
2025-02-14 11:20:10 -0800

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,6 @@ function App() {
   return (
     <>
       <CleanestPools />
-      {/* {filteredPoolTimesLoading ? (
-        <div>Loading...</div>
-      ) : (
-        <div>
-          {filteredPoolTimes.map((pool) => {
-            return (
-              <div key={pool.center_id}>
-                <div>{pool.center_name}</div>
-                {pool.events.map((e, i) => {
-                  return (
-                    <div key={`${e.event_item_id}-${i}`}>
-                      <div>{e.title}</div>
-                      <div>{`${e.start_time} - ${e.end_time}`}</div>
-                    </div>
-                  )
-                })}
-              </div>
-            )
-          })}
-        </div>
-      )} */}
     </>
   )
 }
