ec56b19abb13689ea17dcdd91d72c9e9ece10ec9
shmoo <shmooritchie@gmail.com>
2025-02-14 14:22:13 -0800

    install router dom and set up a basic route

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
 import CleanestPools from './CleanestPools'
+import Pool from './Pool'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <CleanestPools />,
+  },
+  {
+    path: '/pool',
+    element: <Pool />,
+  },
+])
 
 function App() {
-  return <CleanestPools />
+  return <RouterProvider router={router} />
 }
 
 export default App
---
+++ src/Pool.tsx
@@ -0,0 +1,3 @@
+export default function Pool() {
+  return <div> i am a pool </div>
+}
