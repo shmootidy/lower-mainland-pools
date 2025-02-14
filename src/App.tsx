6560acfd75473dcdcf85c113aab906f99beea552
shmoo <shmooritchie@gmail.com>
2025-02-14 12:02:53 -0800

    move query provider to main.tsx from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-
 import './App.css'
 import CleanestPools from './CleanestPools'
 
-const queryClient = new QueryClient()
-
 function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <CleanestPools />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  )
+  return <CleanestPools />
 }
 
 export default App
